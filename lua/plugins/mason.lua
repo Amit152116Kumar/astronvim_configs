797d974d8a5fab066ddb20c8f161a15f83de6cb4
Amit152116Kumar <amit170103004@iitg.ac.in>
2024-11-28 21:15:38 +0530

    added: required lsp, linter & DAP

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- Customize Mason plugins
 
@@ -9,30 +9,45 @@ return {
     "williamboman/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
+      -- Add more LSPs as needed
       ensure_installed = {
         "lua_ls",
-        -- add more arguments for adding more language servers
+        "clangd",
+        "pyright",
+        "gopls",
+        "ast_grep",
       },
     },
   },
+
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
   {
     "jay-babu/mason-null-ls.nvim",
     -- overrides `require("mason-null-ls").setup(...)`
     opts = {
+      -- Add more formatters/linters as needed
       ensure_installed = {
-        "stylua",
-        -- add more arguments for adding more null-ls sources
+        "stylua", -- Lua formatter
+        "black", -- Python formatter
+        "autoflake", -- Python formatter/utility
+        "flake8", -- Python linter
+        "cpplint", -- C++ linter
+        "goimports", -- Go formatter
       },
     },
   },
+
   {
     "jay-babu/mason-nvim-dap.nvim",
     -- overrides `require("mason-nvim-dap").setup(...)`
     opts = {
+      -- Add more debuggers as needed
       ensure_installed = {
-        "python",
-        -- add more arguments for adding more debuggers
+        "codelldb", -- Debugging support for C++
+        "delve", -- Debugging support for Go
+        "cpptools",
+        "dart-debug-adapter",
+        "debugpy",
       },
     },
   },
