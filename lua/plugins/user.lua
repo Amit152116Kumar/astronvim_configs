18ed2e78b8654030d193bce5fc32eccd60dce97b
Amit152116Kumar <amit170103004@iitg.ac.in>
2024-12-25 04:14:08 +0530

    added: copilot plugin

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -10,6 +10,9 @@ return {
 
   "andweeb/presence.nvim",
   {
+    "github/copilot.vim",
+  },
+  {
     "ray-x/lsp_signature.nvim",
     event = "BufRead",
     config = function() require("lsp_signature").setup() end,
