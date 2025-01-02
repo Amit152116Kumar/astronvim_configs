17c98c8f6d4d3e0039c5d36eff540506743c4ce6
Amit152116Kumar <amit170103004@iitg.ac.in>
2025-01-02 12:47:27 +0530

    modified: indent to 4 spaces

--- lua/community.lua
+++ lua/community.lua
@@ -6,12 +6,12 @@
 
 ---@type LazySpec
 return {
-  "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.pack.go" },
-  { import = "astrocommunity.pack.cpp" },
-  { import = "astrocommunity.pack.python" },
-  { import = "astrocommunity.editing-support.todo-comments-nvim" },
-  { import = "astrocommunity.debugging.persistent-breakpoints-nvim" },
-  -- import/override with your plugins folder
+    "AstroNvim/astrocommunity",
+    { import = "astrocommunity.pack.lua" },
+    { import = "astrocommunity.pack.go" },
+    { import = "astrocommunity.pack.cpp" },
+    { import = "astrocommunity.pack.python" },
+    { import = "astrocommunity.editing-support.todo-comments-nvim" },
+    { import = "astrocommunity.debugging.persistent-breakpoints-nvim" },
+    -- import/override with your plugins folder
 }
